1ff88191f57b422e5d351312e33013edca313956
addisonsi <86674993+addisonsi@users.noreply.github.com>
2021-07-07 18:55:52 -0500

    Add files via upload

---
+++ lab10.ino
@@ -0,0 +1,36 @@
+#include <Wire.h>
+SYSTEM_THREAD(ENABLED);
+SYSTEM_MODE(MANUAL);
+char ch;
+// setup() runs once, when the device is first turned on.
+void setup()
+{
+  // Put initialization like pinMode and begin functions here.
+  Serial.begin(9600);
+  while (!Serial.isConnected());
+  
+  Wire.begin();
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop()
+{
+  Serial.println("Test");
+  if(Serial.available()){
+    ch = Serial.read();
+    if(ch != '?'){                                  
+      Wire.beginTransmission(0x2A);
+      Wire.write(ch);
+      Wire.endTransmission();
+    }
+    else{
+      Wire.requestFrom(0x2A, 1);
+      while (Wire.available() == 0);
+      bool value = Wire.read();
+      if(value)
+        Serial.println("HIGH");
+      else
+        Serial.println("LOW");
+    }
+  }
+}
